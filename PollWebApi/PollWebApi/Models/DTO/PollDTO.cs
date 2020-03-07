10541ae8acc10883efee61d1be1b37d30139078b
Leonardo <gomleo9@hotmail.com>
2020-03-07 00:04:56 -0300

    Restaurando arquivos

---
+++ PollWebApi/PollWebApi/Models/DTO/PollDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PollWebApi.Models.DTO
+{
+    public class PollDTO
+    {
+        public int poll_id { get; set; }
+        public string Poll_Description { get; set; }
+        public IEnumerable<PollDetailDTO> Options  { get; set; }
+    }
+}
---
+++ PollWebApi/PollWebApi/Models/DTO/PollDetailDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PollWebApi.Models.DTO
+{
+    public class PollDetailDTO
+    {
+        public int option_id { get; set; }
+        public string option_description { get; set; }
+    }
+}
